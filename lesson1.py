953f915a4850c578b1d98262f2fe50035b24ab19
Ivan2navI <94798058+Ivan2navI@users.noreply.github.com>
2022-06-03 21:15:35 +0300

    first commit

---
+++ lesson1.py
@@ -0,0 +1,59 @@
+
+from time import process_time_ns
+
+
+print(2+4)
+
+print("2"+"4")
+print(5/2)
+print(-5/2)
+
+
+print(round (3.1415, 2)) #Откругление с заданой точностью
+
+
+a = """
+aaaa
+bbbb
+cccc
+"""
+print(a)
+
+print('ssss' *20)
+
+b = 'Hello World'
+print(b[2])
+print(b[-2])
+print(b[2:5]) #Срез
+print(b.capitalize()) #Большие
+
+print (len(b))
+
+s = ["a", "B","crrr"] #Список
+print(s)
+
+print(s[1])
+print(s[2])
+
+print (b.split()) #Резать на список автоматом
+
+t =  ["A", "dd","ddd"]
+d = s +t
+print (t)
+print (d)
+
+s.append (t) #
+print (s)
+s.pop (0) # 
+print (s)
+s.reverse ()
+print (s)
+t.sort()
+print (t)
+
+
+
+
+# ТАПЛ tuple
+b =  ("A", "dd","ddd")
+print(b)
